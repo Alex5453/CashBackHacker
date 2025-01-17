f0afecd59180e848744d13072d3936fa02166820
Alex5453 <130847438+Alex5453@users.noreply.github.com>
2025-01-17 18:50:29 +0300

    Update CashbackHackServiceTest.java

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -9,21 +9,20 @@ public class CashbackHackServiceTest {
     public void shouldReturn100IfAmountIs900() {
         CashbackHackService service = new CashbackHackService();
         int result = service.remain(900);
-        assertEquals(result, 100);
+        assertEquals(100, result);
     }
 
     @Test
     public void shouldReturn0IfAmountIs1000() {
         CashbackHackService service = new CashbackHackService();
         int result = service.remain(1000);
-        assertEquals(result, 0);
+        assertEquals(0, result);
     }
 
     @Test
     public void shouldReturn500IfAmountIs500() {
         CashbackHackService service = new CashbackHackService();
         int result = service.remain(500);
-        assertEquals(result, 500);
+        assertEquals(500, result);
     }
 }
-
