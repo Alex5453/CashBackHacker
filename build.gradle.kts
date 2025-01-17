e206bc58d1056fbba14fd64d45036a531f55c36f
Alex5453 <130847438+Alex5453@users.noreply.github.com>
2025-01-17 18:39:55 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,19 +10,9 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("org.testng:testng:7.1.0")
-    // Remove junit:junit:4.13  - JUnit 4 is not needed with JUnit 5 and TestNG
+    testImplementation("junit:junit:4.13")
 }
 
-tasks.register<Test>("junitJupiterTest") {
-    useJUnitPlatform()
+tasks.test {
     testLogging.showStandardStreams = true //Added for better logging.
-}
-
-tasks.register<Test>("testngTest") {
-    useTestNG()
-    testLogging.showStandardStreams = true //Added for better logging.
-
 }
